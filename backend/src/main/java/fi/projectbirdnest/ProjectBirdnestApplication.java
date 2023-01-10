a8c5523238935d948e7b76de97abef26a375dd43
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-01-10 17:18:12 +0200

    Create DroneService

--- backend/src/main/java/fi/projectbirdnest/ProjectBirdnestApplication.java
+++ backend/src/main/java/fi/projectbirdnest/ProjectBirdnestApplication.java
@@ -1,13 +1,26 @@
 package fi.projectbirdnest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import fi.projectbirdnest.service.DroneService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ProjectBirdnestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ProjectBirdnestApplication.class, args);
+		DroneService droneService = new DroneService(new RestTemplate());
+		droneService.getDrones();
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
---
+++ backend/src/main/java/fi/projectbirdnest/service/DroneService.java
@@ -0,0 +1,43 @@
+package fi.projectbirdnest.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import fi.projectbirdnest.model.Drone;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class DroneService {
+
+    private final RestTemplate restTemplate;
+
+    public List<Drone> getDrones() throws JsonProcessingException {
+        String apiResponse = getDroneSnapshot();
+        return extractDrones(apiResponse);
+    }
+
+    private String getDroneSnapshot() {
+        String uri = "http://assignments.reaktor.com/birdnest/drones";
+        ResponseEntity<String> response = this.restTemplate.getForEntity(uri, String.class);
+        return response.getBody();
+    }
+
+    private List<Drone> extractDrones(String droneReport) throws JsonProcessingException {
+        XmlMapper objectMapper = new XmlMapper();
+        JsonNode body = objectMapper.readTree(droneReport);
+        JsonNode drones = body.get("capture").get("drone");
+        return Arrays.asList(objectMapper.treeToValue(drones, Drone[].class));
+    }
+
+}

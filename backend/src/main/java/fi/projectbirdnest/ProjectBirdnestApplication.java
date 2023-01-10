d64664256acfd94df586c44ea01b85010de2ae64
Röttgen, Valentin <valentin.roettgen@adesso.de>
2023-01-10 10:08:01 +0200

    Initialize Spring project

---
+++ backend/src/main/java/fi/projectbirdnest/ProjectBirdnestApplication.java
@@ -0,0 +1,13 @@
+package fi.projectbirdnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBirdnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBirdnestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fi/projectbirdnest/ProjectBirdnestApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.projectbirdnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBirdnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
